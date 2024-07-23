2836e3318d5bf65342b46ec7c2907cb398692e0d
Dung <tlbgtn@gmail.com>
2024-07-23 19:27:45 +0700

    update: menu dropdown and search form UI

--- resources/js/Components/app/SearchForm.vue
+++ resources/js/Components/app/SearchForm.vue
@@ -1,3 +1,20 @@
+<script setup>
+    import TextInput from '../TextInput.vue';
+    import {useForm} from '@inertiajs/vue3';
+
+    const form = useForm({
+        search: ''
+    });
+</script>
+
 <template>
-    Search Form 
+    <form class="w-[600px] h-[80px] flex items-center">
+        <TextInput 
+            type="text"
+            class="block w-full mr-2"
+            v-model="form.search"
+            autocomplete
+            placeholder="Search for files and folders"
+        />
+    </form>
 </template>
