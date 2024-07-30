02f583babb3c5e69b53a5da0652fb16daa76f010
Dung <tlbgtn@gmail.com>
2024-07-30 21:26:24 +0700

    add: uploading error dialog UI

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -27,6 +27,8 @@
             </template>
         </main>
     </div>
+    <ErrorDialog />
+    <FormProgress :form="fileUploadForm" />
 </template>
 
 <script setup>
@@ -34,8 +36,10 @@
     import Navigation from '@/Components/app/Navigation.vue';
     import SearchForm from '@/Components/app/SearchForm.vue';
     import UserSettingsDropdown from '@/Components/app/UserSettingsDropdown.vue';
-    import { emitter, FILE_UPLOAD_STARTED } from '@/event-bus';
+    import { emitter, FILE_UPLOAD_STARTED, showErrorDialog } from '@/event-bus';
     import { useForm, usePage } from '@inertiajs/vue3';
+    import FormProgress from '@/Components/app/FormProgress.vue';
+    import ErrorDialog from '@/Components/ErrorDialog.vue';
 
     const page = usePage();
 
@@ -71,7 +75,25 @@
         fileUploadForm.files = files;
         fileUploadForm.relative_paths = [...files].map(f => f.webkitRelativePath);
 
-        fileUploadForm.post(route('file.store'));
+        fileUploadForm.post(route('file.store'), {
+            onSuccess: () => {
+
+            },
+            onError: errors => {
+                let message = '';
+                if(Object.keys(errors).length > 0){
+                    message = errors[Object.keys(errors)[0]];
+                }else{
+                    message = 'Error during file upload. Please try again later.'
+                }
+
+                showErrorDialog(message);
+            },
+            onFinish: () => {
+                fileUploadForm.clearErrors();
+                fileUploadForm.reset();
+            }
+        });
     }
 
     onMounted(() => {
